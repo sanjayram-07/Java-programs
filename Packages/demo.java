8296d7e678e6fd65af77f17ec5c7af5429744e67
Sanjay Ram M <m.sanjayram07@gmail.com>
2025-09-20 13:44:37 +0530

    Create demo.java

---
+++ Packages/demo.java
@@ -0,0 +1,16 @@
+import geometry.Rectangle;
+import geometry.Circle;
+public class demo {
+
+    public static void main(String[] args) {
+        
+        Circle c = new Circle(5.0);
+        c.show();
+
+        System.out.println();
+
+        
+        Rectangle r = new Rectangle(4.0, 6.0);
+        r.show();
+    }
+}
